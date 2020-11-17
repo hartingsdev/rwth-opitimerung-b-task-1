0be8b311b0d25a37f2dc149f611c29382e631377
Robert Hartings <robert@hartings.dev>
2020-11-17 16:12:03 +0100

    Commited provided files to git
    I have extraceted the files from the given tarball and commited it to
    the git repository.

---
+++ myShortestPath.cpp
@@ -0,0 +1,13 @@
+#include "myShortestPath.h"
+
+
+std::vector<Vertex> my_shortest_path(const Graph &g, const Vertex &startVertex,
+                                     const Vertex &endVertex) {
+  std::vector<Vertex> path;
+  // compute a shortest path between startVertex and endVertex and save it in
+  // the vector >>path<<
+  //TODO
+
+  // return path
+  return path;
+}
